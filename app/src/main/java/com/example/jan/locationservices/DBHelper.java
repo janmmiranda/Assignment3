f6c2771a0bc52fce040e19c209bba9bfcb1f7bb9
Jan <janm.miranda@gmail.com>
2017-11-16 15:31:34 -0500

    added geocoder

--- app/src/main/java/com/example/jan/locationservices/DBHelper.java
+++ app/src/main/java/com/example/jan/locationservices/DBHelper.java
@@ -6,6 +6,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Jan on 11/12/2017.
  */
@@ -13,6 +17,10 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "MyDBName.db";
+
+    public static final String RELATIONS_TABLE_NAME = "relations";
+    public static final String RELATIONS_COLUMN_ASSOCIATES = "associates";
+
     public static final String COORDINATES_TABLE_NAME = "coordinates";
     public static final String COORDINATES_COLUMN_ID = "id";
     public static final String COORDINATES_COLUMN_NAME = "name";
@@ -25,26 +33,31 @@ public class DBHelper extends SQLiteOpenHelper {
 
 
     public DBHelper(Context context) {
-        super(context,DATABASE_NAME,null,1);
+        super(context,DATABASE_NAME,null,2);
         context.deleteDatabase(DATABASE_NAME);
     }
 
-
+    private static final String CREATE_TABLE_RELATIONS = "CREATE TABLE " + RELATIONS_TABLE_NAME +
+            " (" + COORDINATES_COLUMN_ID + " INTEGER PRIMARY KEY, " + RELATIONS_COLUMN_ASSOCIATES
+            + " TEXT)";
 
     private static final String CREATE_TABLE_COORDINATES = "CREATE TABLE " + COORDINATES_TABLE_NAME +
-            "(" + COORDINATES_COLUMN_ID + " INTEGER PRIMARY KEY," +
-            COORDINATES_COLUMN_NAME + " NAME" + COORDINATES_COLUMN_LONGITUDE + " LONGITUDE," + COORDINATES_COLUMN_LATITUDE +
-            " LATITUDE," + COORDINATES_COLUMN_TIME + " TIME," + COORDINATES_COLUMN_ADDRESS + " ADDRESS" + ")";
+            " (" + COORDINATES_COLUMN_ID + " INTEGER PRIMARY KEY, " +
+            COORDINATES_COLUMN_NAME + " TEXT, " + COORDINATES_COLUMN_LONGITUDE + " TEXT, " + COORDINATES_COLUMN_LATITUDE +
+            " TEXT, " + COORDINATES_COLUMN_TIME + " TEXT, " + COORDINATES_COLUMN_ADDRESS + " TEXT)";
+
+
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-//        db.execSQL("create table coordinates (id integer primary key, name text, longitude text, latitude text, time text, address text)");
         db.execSQL(CREATE_TABLE_COORDINATES);
+        db.execSQL(CREATE_TABLE_RELATIONS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion1) {
         db.execSQL("DROP TABLE IF EXISTS" +  COORDINATES_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS" +  RELATIONS_TABLE_NAME);
         onCreate(db);
     }
 
@@ -58,6 +71,60 @@ public class DBHelper extends SQLiteOpenHelper {
         contentValues.put("address", address);
         db.insert("coordinates", null, contentValues);
         return true;
+    }
+
+    public boolean insertRelation(String relate) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("associates", relate);
+        return true;
+    }
+
+    public HashMap<String, String> getCoord(int i) {
+        HashMap<String, String> coordinate = new HashMap<String, String>(2);
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from coordinates where id = '" + i + "'", null);
+        res.moveToFirst();
+        String name, longitude, latitude, time, address, id;
+        if(res.moveToFirst()) {
+            do {
+                name = res.getString(res.getColumnIndex(COORDINATES_COLUMN_NAME));
+                longitude = res.getString(res.getColumnIndex(COORDINATES_COLUMN_LONGITUDE));
+                latitude = res.getString(res.getColumnIndex(COORDINATES_COLUMN_LATITUDE));
+                time = res.getString(res.getColumnIndex(COORDINATES_COLUMN_TIME));
+                address = res.getString(res.getColumnIndex(COORDINATES_COLUMN_ADDRESS));
+                coordinate.put("name", name);
+                coordinate.put("longitude", longitude);
+                coordinate.put("latitude", latitude);
+                coordinate.put("time", time);
+                coordinate.put("address", address);
+            } while(res.moveToNext());
+        }
+        return coordinate;
+    }
+
+    public ArrayList<Map<String, String>> getAllCoords() {
+        ArrayList<Map<String, String>> array_List = new ArrayList<Map<String, String>>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery( "select * from coordinates", null);
+        res.moveToFirst();
+        String name, longitude, latitude, time, address, id;
+        while(res.isAfterLast() == false) {
+            HashMap<String, String> coordinates = new HashMap<String, String>(2);
+            name = res.getString(res.getColumnIndex(COORDINATES_COLUMN_NAME));
+            longitude = res.getString(res.getColumnIndex(COORDINATES_COLUMN_LONGITUDE));
+            latitude = res.getString(res.getColumnIndex(COORDINATES_COLUMN_LATITUDE));
+            time = res.getString(res.getColumnIndex(COORDINATES_COLUMN_TIME));
+            address = res.getString(res.getColumnIndex(COORDINATES_COLUMN_ADDRESS));
+            coordinates.put("name", name);
+            coordinates.put("longitude", longitude);
+            coordinates.put("latitude", latitude);
+            coordinates.put("time", time);
+            coordinates.put("address", address);
+            array_List.add(coordinates);
+            res.moveToNext();
+        }
+        return array_List;
     }
 
 }
--- app/src/main/java/com/example/jan/locationservices/MainActivity.java
+++ app/src/main/java/com/example/jan/locationservices/MainActivity.java
@@ -1,30 +1,37 @@
 package com.example.jan.locationservices;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.math.MathUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.identity.intents.Address;
 import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
@@ -36,17 +43,29 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private double mylongitude;
     private double mylatitude;
     private long mytime;
-    private Geocoder geocode;
-    public List<android.location.Address> myList = null;
+    private Geocoder mylocation;
+    private SimpleAdapter addressAdapter;
+    public List<Address> myAddress;
+    private ListView addressList;
+    private ArrayList<Map<String, String>> addresses;
+    String[] from = {"name", "time", "longitude", "latitude", "address"};
+    int[] to = {R.id.addrName, R.id.addrTime, R.id.addrLong, R.id.addrLat, R.id.address};
     DBHelper mydb;
+    int addrCount = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("String", "onCreate: this runs");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mydb = new DBHelper(this);
+
+        addressList = (ListView) findViewById(R.id.addressList);
+        addresses = mydb.getAllCoords();
+        addressAdapter = new SimpleAdapter(this, addresses, R.layout.list_viewadater, from, to);
+        addressList.setAdapter(addressAdapter);
+
+        mylocation = new Geocoder(this, Locale.getDefault());
 
         latitudeValue = (TextView) findViewById(R.id.latVal);
         longitudeValue = (TextView) findViewById(R.id.lngVal);
@@ -64,17 +83,77 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     }
 
-    public void onCheckIn(View v) {
-        EditText et = (EditText) findViewById(R.id.editText);
-        String name = String.valueOf(et);
-        int maxResults = 1;
-        try {
-            myList = geocode.getFromLocation(mylatitude, mylongitude, maxResults);
-        } catch (IOException e) {
-            e.printStackTrace();
+    private boolean checkDistance() {
+        if (addrCount < 2) {
+            return false;
+        } else {
+            int temp = addrCount;
+            while (temp >= 1) {
+                HashMap<String, String> coord1 = new HashMap<String, String>(2);
+                coord1 = mydb.getCoord(addrCount);
+                if(distanceChecker(coord1)) {
+                    return true;
+                }
+                temp--;
+            }
         }
-        mydb.insertCD(name, String.valueOf(mylongitude), String.valueOf(mylatitude), String.valueOf(mytime), String.valueOf(myList));
-        Toast.makeText(this, "Sent from " + String.valueOf(myList), Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    private boolean distanceChecker(HashMap coord1) {
+        long lat1 = (long) coord1.get("latitude");
+        long lat2 = (long) mylatitude;
+        long lng1 = (long) coord1.get("longitude");
+        long lng2 = (long) mylongitude;
+
+        double varR = 6371e3;
+        double var1 = Math.toRadians(lat1);
+        double var2 = Math.toRadians(lat2);
+        double var3 = Math.toRadians(lat2 - lat1);
+        double var4 = Math.toRadians(lng2 - lng1);
+
+        double varA = Math.sin(var3/var2) * Math.sin(var3/var2) +
+                Math.cos(var1) * Math.cos(var2) * Math.sin(var4/2) * Math.sin(var4/2);
+        double varC = 2 * Math.atan2(Math.sqrt(varA), Math.sqrt(varA));
+        double varD = varR * varC;
+
+        if(varD/1000 <= 30) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private Address getAddress() {
+        try {
+            myAddress = mylocation.getFromLocation(mylatitude, mylongitude, 1);
+        } catch (IOException e) {
+            return null;
+        }
+        if (myAddress != null && !myAddress.isEmpty())
+            return myAddress.get(0);
+        else
+            return null;
+    }
+
+    public void onCheckIn(View v) {
+        EditText et = (EditText) findViewById(R.id.etNewAddress);
+        String name = et.getText().toString();
+        Address tempAddress = getAddress();
+        String address = String.valueOf(tempAddress.getLocality());
+        mydb.insertCD(name, String.valueOf(mylongitude), String.valueOf(mylatitude), String.valueOf(mytime), address);
+
+        addresses = mydb.getAllCoords();
+        addressAdapter = new SimpleAdapter(this, addresses, R.layout.list_viewadater, from, to);
+        addressList.setAdapter(addressAdapter);
+
+//        if(checkDistance()) {
+//
+//        }
+        //checkDistance();
+        et.setText("");
+        addrCount++;
+        Toast.makeText(this, "Sent at " + String.valueOf(mytime), Toast.LENGTH_LONG).show();
     }
 
     @Override
